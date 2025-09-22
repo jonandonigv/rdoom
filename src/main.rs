f866c44eadc061f1e8a97d5ee28342165dbf4411
jonandonigv <jonandonigv@gmail.com>
2025-09-22 16:26:21 +0200

    Creates 800x600 window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate sdl2;
+use std::time::Duration;
+
+use sdl2::{event::Event, keyboard::Keycode, pixels::Color};
+
 mod assets;
 mod combat;
 mod enemy;
@@ -6,5 +11,37 @@ mod player;
 mod renderer;
 
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("rdoom", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+    }
+
+    canvas.present();
+    ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
 }
