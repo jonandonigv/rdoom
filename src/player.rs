fa09b697bddb6c51f043b90997161527ffcdc667
jonandonigv <jonandonigv@gmail.com>
2025-09-23 23:14:27 +0200

    wip: Started working on player feature
    Created player struct, handle player input, get player_pos. TODO: update
    player position and add collision to player

---
+++ src/player.rs
@@ -0,0 +1,72 @@
+use sdl2::{event::Event, keyboard::Keycode};
+
+pub struct Player {
+    x: f32,
+    y: f32,
+    speed: f32,
+    moving_up: bool,
+    movind_down: bool,
+    moving_right: bool,
+    moving_left: bool,
+}
+
+impl Player {
+    fn new(x: f32, y: f32) -> Self {
+        Player {
+            x,
+            y,
+            speed: 5.0,
+            moving_up: false,
+            movind_down: false,
+            moving_right: false,
+            moving_left: false,
+        }
+    }
+
+    pub fn handle_event(&mut self, event: &Event) {
+        match event {
+            Event::KeyDown {
+                keycode: Some(key), ..
+            } => match *key {
+                Keycode::W => self.moving_up = true,
+                Keycode::S => self.movind_down = true,
+                Keycode::A => self.moving_left = true,
+                Keycode::D => self.moving_right = true,
+                _ => {}
+            },
+            Event::KeyUp {
+                keycode: Some(key), ..
+            } => match *key {
+                Keycode::W => self.moving_up = false,
+                Keycode::S => self.movind_down = false,
+                Keycode::A => self.moving_left = false,
+                Keycode::D => self.moving_right = false,
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+
+    pub update(&mut self, &crate::map::Map, delta_time: f32) {
+        let mut dx = 0.0;
+        let mut dy = 0.0;
+        let move_dist = self.speed * delta_time;
+        
+        if self.moving_up {
+            dy -= move_dist;
+        }
+        if self.movind_down {
+            dy += move_dist;
+        }
+        if self.moving_right {
+            dx += move_dist;
+        }
+        if self.moving_left {
+            dx -= moving_left
+        }
+    }
+
+    pub fn get_position(&self) -> (f32, f32) {
+        (self.x, self.y)
+    }
+}
