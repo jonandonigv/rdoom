58f4dc32d1dc69357a52b3d23d5f3460970bd523
jonandonigv <jonandonigv@gmail.com>
2025-09-26 21:40:31 +0200

    wip: What is raycasting

--- src/renderer/raycast.rs
+++ src/renderer/raycast.rs
@@ -1,5 +1,8 @@
 pub struct Raycast;
 
+// What is Raycasting?
+// Raycasting is a rendering technique to create a 3d perspective in a 2d map.
+
 impl Raycast {
     pub fn new() -> Self {
         Raycast
