0eb4f533b269e78069b87215e238c09b83b6f0d2
jonandonigv <jonandonigv@gmail.com>
2025-09-27 19:18:41 +0200

    wip: raycasting tutorial

--- src/renderer/raycast.rs
+++ src/renderer/raycast.rs
@@ -1,5 +1,6 @@
 pub struct Raycast;
 
+// TODO: Read the Raycasting build your own x tutorial
 // What is Raycasting?
 // Raycasting is a rendering technique to create a 3d perspective in a 2d map.
 
